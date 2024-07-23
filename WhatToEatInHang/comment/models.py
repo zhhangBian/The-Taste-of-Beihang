975d363af82389232e01bf2fd864dafa2d7b2389
Congrong Li <1955706051@qq.com>
2024-07-23 22:42:21 +0800

    create 'users', add models to 'restaurant'

--- WhatToEatInHang/comment/models.py
+++ WhatToEatInHang/comment/models.py
@@ -7,6 +7,7 @@ class Comment(models.Model):
     """
     content = models.CharField(max_length=200)
     comment_time = models.DateTimeField(auto_now_add=True)
+    grade = models.IntegerField(default=0, choices=((0, '未评分'), (1, '一星'), (2, '二星'), (3, '三星'), (4, '四星'), (5, '五星')))
 
     # refer_post = models.ForeignKey('Post', on_delete=models.CASCADE)
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
--- WhatToEatInHang/restaurant/models.py
+++ WhatToEatInHang/restaurant/models.py
@@ -27,3 +27,17 @@ class Dish(models.Model):
     dish_img = models.ImageField(upload_to='dish/', default='dish/default.png', verbose_name='dish picture')
 
 
+class Post(models.Model):
+    """
+    posts under restaurants
+    """
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=2000)
+    grade = models.IntegerField(default=0, choices=((0, '未评分'), (1, '一星'), (2, '二星'), (3, '三星'), (4, '四星'), (5, '五星')))
+    avg_price = models.IntegerField(default=0)
+    restaurant = models.ForeignKey('Restaurant', related_name='posts', on_delete=models.CASCADE)
+    creator = models.ForeignKey('users.User', related_name='posts', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    agrees = models.ManyToManyField('users.User', related_name='agreedPosts')
+    # image = models.ImageField(upload_to='post_image/', default='post_image/default.png')
+    # recommend_dish = models.ManyToManyField('Dish', related_name='recommended_by', through='RecommendDish')
