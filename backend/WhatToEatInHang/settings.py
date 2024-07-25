64dbfb315d49e6f5137e9ec3bb230acd4bfff978
Alex Bian <2317164114@qq.com>
2024-07-26 00:37:56 +0800

    添加文件路径

--- backend/WhatToEatInHang/settings.py
+++ backend/WhatToEatInHang/settings.py
@@ -34,6 +34,13 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 ROOT_URLCONF = "WhatToEatInHang.urls"
 
 TEMPLATES = [
