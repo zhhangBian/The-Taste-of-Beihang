71f49ec2abf12239f9050c6cf28dcb259d225c7e
Jew <22373498@buaa.edu.cn>
2024-07-26 15:38:02 +0800

    构建登录注册页面，待调试

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,65 +1,263 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-             target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-             target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"
-             rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank"
-             rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="box">
+    <div class="content" :class="{ 'add-class-content': !isLogin }">
+      <img class="login-img images" src="@/assets/login.jpg" alt="登录" :class="{ 'add-class-login-img': !isLogin, 'add-class-login-img-show': isLogin }">
+      <img class="register-img images" src="@/assets/register.jpg" alt="注册" :class="{ 'add-class-register-img': !isLogin }">
+      <div class="login-wrapper" :style="{ height: isLogin ? '70vh' : '80vh' }">
+        <div class="top-tips">
+          <span class="cat-text">CAT IS LITTLE #FFF</span>
+        </div>
+        <h1 class="h1-text">{{ isLogin ? '登录' : '注册' }}</h1>
+        <div class="form-container">
+          <div class="login-form" v-show="isLogin">
+            <div class="user-form form-item">
+              <div class="text-tips">
+                <span>账号</span>
+              </div>
+              <input type="text">
+            </div>
+            <div class="password-form form-item">
+              <div class="text-tips">
+                <span>密码</span>
+              </div>
+              <input type="password">
+            </div>
+            <button class="btn">登录</button>
+            <button class="toggle-btn" @click="toggleForm">没有账户？</button>
+          </div>
+          <div class="register-form" v-show="!isLogin">
+            <div class="user-form form-item">
+              <div class="text-tips">
+                <span>账号</span>
+              </div>
+              <input type="text">
+            </div>
+            <div class="password-form form-item">
+              <div class="text-tips">
+                <span>密码</span>
+              </div>
+              <input type="password">
+            </div>
+            <div class="password-form form-item">
+              <div class="text-tips">
+                <span>确认密码</span>
+              </div>
+              <input type="password">
+            </div>
+            <button class="btn">注册</button>
+            <button class="toggle-btn" @click="toggleForm">已有账户？</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      isLogin: true
+    };
+  },
+  methods: {
+    toggleForm() {
+      this.isLogin = !this.isLogin;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
+* {
+  margin: 0;
   padding: 0;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.box {
+  background-color: rgb(51, 32, 108);
+  width: 100vw;
+  height: 100vh;
+}
+.box .content {
+  width: 90vw;
+  height: 85vh;
+  background-color: rgb(228, 218, 232);
+  border-radius: 40px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  transition: 1s;
+}
+.box .content .images {
+  margin-left: -150px;
+  margin-right: 100px;
+  position: absolute;
+  left: 15%;
+}
+.box .content .login-img {
+  width: 45%;
+  height: 90%;
+  border-radius: 15%;
+}
+.box .content .register-img {
+  width: 45%;
+  opacity: 0;
+  border-radius: 15%;
+}
+.box .content .login-wrapper {
+  width: 30vw;
+  height: 70vh;
+  border-radius: 40px;
+  -webkit-backdrop-filter: blur(1px);
+          backdrop-filter: blur(1px);
+  background: rgba(244, 240, 245, 0.6);
+  padding: 60px;
+  box-sizing: border-box;
+  position: absolute;
+  right: 10%;
+  transition: 1s;
+}
+.box .content .login-wrapper .top-tips {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 20px;
+}
+.box .content .login-wrapper .top-tips .cat-text {
+  font-size: 25px;
+  font-weight: 600;
+}
+.box .content .login-wrapper h1 {
+  font-size: 45px;
+  margin: 20px 0;
+  text-align: left;
+}
+.box .content .login-wrapper .login-form,
+.box .content .login-wrapper .register-form {
+  width: 100%;
+}
+.box .content .login-wrapper .login-form .form-item .text-tips,
+.box .content .login-wrapper .register-form .form-item .text-tips {
+  color: rgb(123, 122, 123);
+  margin: 5px;
+  text-align: left;
+}
+.box .content .login-wrapper .login-form .form-item input,
+.box .content .login-wrapper .register-form .form-item input {
+  width: 100%;
+  height: 50px;
+  margin: 5px 0;
+  border-radius: 5px;
+  border: 0;
+  font-size: 17px;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.box .content .login-wrapper .login-form .form-item input:focus,
+.box .content .login-wrapper .register-form .form-item input:focus {
+  outline: none;
+  border: 1px solid rgb(79, 133, 226);
+}
+.box .content .login-wrapper .login-form .btn,
+.box .content .login-wrapper .register-form .btn {
+  width: 100%;
+  height: 50px;
+  margin: 20px 0;
+  background-color: rgb(59, 58, 59);
+  border: 0;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  text-align: center;
+}
+.box .content .login-wrapper .login-form .toggle-btn,
+.box .content .login-wrapper .register-form .toggle-btn {
+  width: 100%;
+  height: 50px;
+  margin: 10px 0;
+  background-color: transparent;
+  border: 0;
+  color: rgb(79, 133, 226);
+  font-size: 15px;
+  cursor: pointer;
+  text-align: center;
+}
+.form-container {
+  display: block;
 }
 
-a {
-  color: #42b983;
+.add-class-content {
+  background-color: rgb(156, 148, 208) !important;
+  transition: 0.1s;
+}
+
+.add-class-login-img {
+  -webkit-animation: disappear 0.3s forwards;
+          animation: disappear 0.3s forwards;
+  -webkit-animation-timing-function: ease;
+          animation-timing-function: ease;
+}
+
+.add-class-login-img-show {
+  -webkit-animation-delay: 1s;
+          animation-delay: 1s;
+  -webkit-animation: appear 1s forwards;
+          animation: appear 1s forwards;
+  -webkit-animation-timing-function: ease;
+          animation-timing-function: ease;
+}
+
+.add-class-register-img {
+  -webkit-animation: appear 1s forwards;
+          animation: appear 1s forwards;
+  -webkit-animation-timing-function: ease;
+          animation-timing-function: ease;
+  -webkit-animation-delay: 0.2s;
+          animation-delay: 0.2s;
+}
+
+@-webkit-keyframes appear {
+  from {
+    border-radius: 15%;
+    opacity: 0;
+  }
+  to {
+    border-radius: 0%;
+    opacity: 1;
+  }
+}
+
+@keyframes appear {
+  from {
+    border-radius: 15%;
+    opacity: 0;
+  }
+  to {
+    border-radius: 0%;
+    opacity: 1;
+  }
+}
+@-webkit-keyframes disappear {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+}
+@keyframes disappear {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
 }
 </style>
