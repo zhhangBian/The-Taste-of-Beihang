4e184c8b22551568d32a75fa83485be9211daf93
Jew <22373498@buaa.edu.cn>
2024-07-31 00:43:10 +0800

    解决已知bug

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,27 @@ import "./assets/common.css";
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
+const debounce = (fn, delay) => {
+    let timer = null;
+    return function () {
+      let context = this;
+      let args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, delay);
+    }
+  }
+  
+  const _ResizeObserver = window.ResizeObserver;
+  window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+    constructor(callback) {
+      callback = debounce(callback, 16);
+      super(callback);
+    }
+  }
+  
+
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
